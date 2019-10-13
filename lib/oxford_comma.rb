832078b57a18d1b1597f278ddf4808487b4d3bd1
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 17:09:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,32 @@
 def oxford_comma(array)
+  formatted_string_string = "apples,bananas,pineapples"
+  array.each_with_index{ |word,each_with_index
+    if(index ==0)
+      formatted_string < word
+    end
+  }
+end
 
+def oxford_comma(array)
+  formatted_string ="apples,bananas,pineapples"
+  array.each_with_index{ |word, index
+    if(index==0)
+      formatted_string << word
+    else
+      formatted_string <<",#{word}"
+    end
+  }
+  formatted_string
+end
+
+def oxford_comma(array)
+  formatted_string ="apples,bananas,pineapples"
+  array.each_with_index{ |and, index
+    if(index==0)
+      formatted_string << word
+    elsif(index>0 && index<(array.lenght - 1))
+      formatted_string <<",#{word}"
+    end
+  }
+  formatted_string
 end
